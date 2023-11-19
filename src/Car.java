10b7b64fc28434719b3f8182b87a78c71aeaf5a3
TomaszM-dev <tomekmalocha@gmail.com>
2023-11-19 16:42:23 +0100

    oop

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    String make = "BMW";
+    String model = "e60";
+    int year = 2020;
+    String color= "blue";
+    double price = 40000.00;
+
+    void drive(){
+        System.out.println("Your drive the car");
+    }
+
+    void brake(){
+        System.out.println("Your braked");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
 
-    System.out.println("what is your name ?");
-    String name = scanner.nextLine();
 
-        System.out.println("what is your age ?");
-    int age = scanner.nextInt();
 
-    System.out.println(age);
 
     }
+
+
 }
